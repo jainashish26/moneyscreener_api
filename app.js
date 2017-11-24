3b551f74b360ee9ed331787abc1793f7829c2200
ASHISH JAIN <gmail@jainashish.com>
2017-11-24 15:35:26 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+
+var logger = function(req, res, next){
+  console.log('Logging...');
+  next();
+}
+
+app.use(logger);
+
+//Set static path for source data
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/:id', function (req, res) {
+   //Read stock market data: stocksdata.csv
+   fs.readFile( __dirname + "/public/" + "aboutmarkets.csv", 'utf8', function (err, data) {
+     var allTextLines = data.split(/\r\n|\n/);
+     var headers = allTextLines[0].split(',');
+     var lines = [];
+
+     for (var i=1; i<allTextLines.length; i++) {
+         var stockdata = allTextLines[i].split(',');
+         if (stockdata.length == headers.length) {
+            if (stockdata[0] == req.params.id || stockdata[1] == req.params.id ) {
+              console.log(allTextLines[i]);
+              res.send(allTextLines[i]);
+              return;
+            }
+         }
+     }
+   });
+});
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("App listening at http://%s:%s", host, port);
+});
